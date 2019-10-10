09efe1bc49152d4541a010e264befd1dfe868d43
afifarn <afifr0837@gmail.com>
2019-10-10 19:07:09 +0700

    first commit

---
+++ src/main/java/PBOUlang_10116138_Latihan21_RataRataNilai/RataRataNilai.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBOUlang_10116138_Latihan21_RataRataNilai;
+import java.util.Scanner;
+/**
+ *
+ * @author 
+ * NAMA     : Afif Raditya Nugraha
+ * KELAS    : PBO_ULANG/IF4
+ * NIM      : 10116138
+ * LOG      : Program ini berisi program untuk Menampilkan Rata-Rata Nilai
+ * NORunning
+ */
+public class RataRataNilai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        int nBanyak, jml, nilai[];
+        float rata, jumlah = 0;
+
+        System.out.print("Masukan Jumlah Mahasiswa : ");
+        nBanyak = input.nextInt();
+
+        nilai = new int[nBanyak];
+
+        for (int i = 0; i <= nBanyak - 1; i++) {
+            System.out.print("Nilai Mahasiswa Ke-" + (i + 1) + " : ");
+            nilai[i] = input.nextInt();
+        }
+        for (int j = 0; j <= nBanyak - 1; j++) {
+            jumlah = jumlah + nilai[j];
+        }
+
+        rata = jumlah / nBanyak;
+        System.out.println();
+        System.out.println("Maka, rata rata nilainya adalah " + rata);
+        System.out.println("Devoloped by  Afif Nugraha");
+    }
+    }
+   
